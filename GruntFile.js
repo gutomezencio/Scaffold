a70b32ca6be5ae7ea1a6346a972a810e828b44fd
Marcos Moura <marcosviniciusmoura2005@gmail.com>
2013-10-28 04:11:02 -0200

    Adiciona Time Elapsed ao Scaffold

--- GruntFile.js
+++ GruntFile.js
@@ -28,6 +28,7 @@ module.exports = function(grunt) {
         };
 
     require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
